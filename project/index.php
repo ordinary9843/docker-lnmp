74072a3a7d0bc7f6b3d4783f7370b8cbc89f8fee
jerrychen <jerrychen@jesda.com.tw>
2020-12-16 17:32:22 +0800

    [feat] 可正常連接 redis

--- project/index.php
+++ project/index.php
@@ -1,2 +1,21 @@
 <?php
-phpinfo();
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+try {
+    $dbName = 'performance_schema';
+    $username = 'root';
+    $password = '123456';
+
+    $conn = new PDO('mysql:host=mysql:3306;dbname=' . $dbName, $username, $password);
+
+    echo 'mysql 連接成功<br>';
+
+    $redis = new \Redis();
+    $redis->connect('redis','6379');
+    $redis->auth('123456');
+
+    echo 'redis 連接成功<br>';
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
