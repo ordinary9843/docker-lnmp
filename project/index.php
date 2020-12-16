6d006118835891ff5d073c420e39cc03f9d2b899
jerrychen <jerrychen@jesda.com.tw>
2020-12-16 16:13:06 +0800

    [feat] 刪除測試用專案

--- project/index.php
+++ project/index.php
@@ -1,23 +1,2 @@
 <?php
-try {
-    echo ini_get('memory_limit') . '<br>';
-
-    $dbname = 'performance_schema';
-    $username = 'root';
-    $password = '123456';
-
-    $conn = new PDO('mysql:host=mysql:3306;dbname=' . $dbname, $username, $password);
-
-    echo '已連接資料庫<br>';
-
-    $stmt = $conn->prepare('SELECT * FROM accounts');
-    $stmt->execute();
-    $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $result = $stmt->fetchAll();
-
-    foreach ($result as $key => $value) {
-        echo $value['USER'] . '<br>';
-    }
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
+phpinfo();
